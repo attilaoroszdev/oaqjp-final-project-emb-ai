5f4a72357749c396c2572d90fd5c4b5bab3f5e8b
Attila Orosz <attila.orosz.dev@gmail.com>
2025-09-03 13:50:42 +0400

    Task 5 fixed

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -10,10 +10,16 @@ class TestSentimentAnalyzer(unittest.TestCase):
         test_case_4 = {'text': 'I am so sad about this', 'result': 'sadness'}
         test_case_5 = {'text': 'I am really afraid that this will happen', 'result': 'fear'}
 
-        self.assertEqual(emotion_detector(test_case_1['text']), test_case_1['result'])
-        self.assertEqual(emotion_detector(test_case_2['text']), test_case_2['result'])
-        self.assertEqual(emotion_detector(test_case_3['text']), test_case_3['result'])
-        self.assertEqual(emotion_detector(test_case_4['text']), test_case_4['result'])
-        self.assertEqual(emotion_detector(test_case_5['text']), test_case_5['result'])
+        result_1 = emotion_detector(test_case_1['text'])
+        self.assertEqual(result_1['dominant_emotion'], test_case_1['result'])
+        result_2 = emotion_detector(test_case_2['text'])
+        self.assertEqual(result_2['dominant_emotion'], test_case_2['result'])
+        result_3 = emotion_detector(test_case_3['text'])
+        self.assertEqual(result_3['dominant_emotion'], test_case_3['result'])
+        result_4 = emotion_detector(test_case_4['text'])
+        self.assertEqual(result_4['dominant_emotion'], test_case_4['result'])
+        result_5 = emotion_detector(test_case_5['text'])
+        self.assertEqual(result_5['dominant_emotion'], test_case_5['result'])
+    
 
 unittest.main()
