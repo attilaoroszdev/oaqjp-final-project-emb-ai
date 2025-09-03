e27998dcce8296a3afe55fa9bbe0ba7528737f34
Attila Orosz <attila.orosz.dev@gmail.com>
2025-09-03 16:19:57 +0400

    Something was missing...

--- server.py
+++ server.py
@@ -27,6 +27,9 @@ def detect_emotion():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
+    if result['dominant_emotion'] is None:
+        return "Invalid input! Please try again."
+
     formatted_result = (
         f"For the given statement, the system response is 'anger': {result['anger']}, "
         f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} "
